3432ebf249088c8f4eeebb7b4e9d4c2de8803fee
Trevor Rounce <trevorrounce@Trevors-iMac.hitronhub.home>
2019-09-13 14:52:22 -0600

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,12 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
-import styled from 'styled-components'
-
-import PostListing from '../components/Posts/PostListing'
-
-import Img from 'gatsby-image'
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>{data.site.siteMetadata.title}</h1>
     <p style={{ fontFamily: 'Homemade Apple' }}>
       {data.site.siteMetadata.description}
     </p>
@@ -57,14 +50,6 @@ const IndexPage = ({ data }) => (
       sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl.
       Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna.
     </p>
-    <p>By {data.site.siteMetadata.author}</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    {data.allMarkdownRemark.edges.map(({ node }, index) => {
-      return <PostListing key={node.id} post={node} />
-    })}
-    <Img fluid={data.imageOne.childImageSharp.fluid} />
   </Layout>
 )
 
@@ -87,26 +72,6 @@ export const query = graphql`
         title
         author
         description
-      }
-    }
-
-    imageOne: file(relativePath: { eq: "images/CAS-paint-background.jpg" }) {
-      ...fluidImage
-    }
-
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM YYYY")
-          }
-          fields {
-            slug
-          }
-          html
-        }
       }
     }
   }
